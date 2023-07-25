169c84510bdef3577e1a4030a6f8ff588de39150
Kelly Plyler <kellysplyler@gmail.com>
2023-07-25 15:57:41 -0500

    finished Selection Statements

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Guess what my favorite number is!");
+            int userGuess = int.Parse(Console.ReadLine());
+            int favNumber = 13;
+            {
+                if (userGuess == favNumber)
+                { Console.WriteLine("You guessed it!"); 
+                
+               }
+                else if (userGuess > favNumber)
+                    {
+                    Console.WriteLine("Too high");
+                }
+                else
+                {
+                    Console.WriteLine("Too low");
+                }
+
+                        }
         }
     }
 }
